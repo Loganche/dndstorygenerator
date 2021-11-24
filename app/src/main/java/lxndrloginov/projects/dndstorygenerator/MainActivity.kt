6a1b33b5e6b166d46b7cccedf8b4537de8e798e4
Alexander Loginov <lxndrloginov@gmail.com>
2021-11-24 12:42:24 +0300

    Default files

---
+++ app/src/main/java/lxndrloginov/projects/dndstorygenerator/MainActivity.kt
@@ -0,0 +1,38 @@
+package lxndrloginov.projects.dndstorygenerator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import lxndrloginov.projects.dndstorygenerator.ui.theme.DndstorygeneratorappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DndstorygeneratorappTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DndstorygeneratorappTheme {
+        Greeting("Android")
+    }
+}
